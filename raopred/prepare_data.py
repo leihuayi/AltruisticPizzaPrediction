77daa21f147825a1def39ad9f5c3d6d621f3cfa1
Lei Hua Yi <leihuayi@gmail.com>
2022-04-02 15:37:30 +0200

    Start organizing code

---
+++ raopred/prepare_data.py
@@ -0,0 +1,49 @@
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+punctuation = list(",.?!(){}[]-_\"'\\;:+*<>@#§^$%&|/") + ['\n', '\r', '\t', '...', '..']
+stop_words = set(stopwords.words('english'))
+stop_words.add("request")
+stop_words.add("edit")
+
+lemmatizer = WordNetLemmatizer()
+tag_dict = {"J": wn.ADJ,
+            "N": wn.NOUN,
+            "V": wn.VERB,
+            "R": wn.ADV}
+
+
+def extract_wnpostag_from_postag(tag):
+    #take the first letter of the tag
+    #the second parameter is an "optional" in case of missing key in the dictionary 
+    return tag_dict.get(tag[0].upper(), None)
+
+
+def lemmatize_tupla_word_postag(tupla):
+    """
+    giving a tupla of the form (wordString, posTagString) like ('guitar', 'NN'), return the lemmatized word
+    """
+    tag = extract_wnpostag_from_postag(tupla[1])    
+    return lemmatizer.lemmatize(tupla[0], tag) if tag is not None else tupla[0]
+
+
+def correspondance_miswrite(word):
+    if word == "im":
+        return "i'm"
+    elif word == "ive":
+        return "i've"
+
+
+def clean_text(sentence):
+    sentence = sentence.lower()
+    original_words = word_tokenize(sentence)
+    tagged_words = nltk.pos_tag(original_words) #returns a list of tuples: (word, tagString) like ('And', 'CC')
+    lemmatized_words = [ lemmatize_tupla_word_postag(ow) for ow in tagged_words ]
+    cleaned_words = [ 
+        w for w in lemmatized_words if (w not in punctuation) and (w not in stop_words)
+    ]
+    cleaned_words = [ correspondance_miswrite(w) for w in cleaned_words ]
+    return ' '.join(cleaned_words)
---
+++ research/train.py
@@ -0,0 +1,79 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from raopred.prepare_data import clean_text
+
+
+vectorizer = TfidfVectorizer(lowercase=False, max_features=50)
+
+
+def clean_col(row, col):
+    return clean_text(row[col])
+
+
+def get_data(df):
+    return df.apply(lambda x: clean_col(x, 'request_title'), axis =1)
+
+
+def get_labels(df):
+    return np.array(df['requester_received_pizza']).astype(int)
+
+
+def get_train_test_split(data, labels, split=0.15):
+    X_train, X_test, y_train, y_test = train_test_split(data, 
+                                                        labels, 
+                                                        test_size=split, 
+                                                        random_state=8)
+
+    vectorizer = TfidfVectorizer(stop_words=None,
+                            lowercase=False,
+                            max_df=1,
+                            min_df=10,
+                            max_features=200,
+                            norm='l2',
+                            sublinear_tf=True)
+                            
+    features_train = vectorizer.fit_transform(X_train).toarray()
+    features_test = vectorizer.transform(X_test).toarray()
+    # features_names = vectorizer.get_feature_names_out()
+
+    return (features_train, features_test, y_train, y_test)
+
+
+def evaluate(model, X_test, y_test):
+    pred = model.predict(X_test)
+    print('The test accuracy is: ', accuracy_score(y_test, pred))
+    print('Classification report')
+    print(classification_report(y_test, pred))
+    conf_matrix = confusion_matrix(y_test, pred, normalize='true')
+    print(conf_matrix)
+
+
+def train(model, X_train, y_train):
+    model.fit(X_train, y_train)
+    return model
+
+
+def training_pipeline():
+    df = pd.read_json(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'pizza_data.json')
+        ,orient='values')
+    print('Extracting and preparing data for training ...')
+    data = get_data(df)
+    labels = get_labels(df)
+    X_train, X_test, y_train, y_test = get_train_test_split(data, labels)
+
+    print('Train model ...')
+    model = svm.SVC(kernel='linear')
+    train(model, X_train, y_train)
+
+    print('Evaluate model...')
+    evaluate(model, X_test, y_test)
+
+if __name__=="__main__":
+    training_pipeline()
